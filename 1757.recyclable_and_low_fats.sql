0590f3e5840a61bb9a33c7f72fd88667281c09ee
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2026-06-21 02:39:24 +0530

    Update 1757.recyclable_and_low_fats.sql

--- 1757.recyclable_and_low_fats.sql
+++ 1757.recyclable_and_low_fats.sql
@@ -42,4 +42,6 @@ Output:
 +-------------+
 Explanation: Only products 1 and 3 are both low fat and recyclable.
 */
-select product_id from Products where low_fats='Y' and recyclable='Y';
+# Write your MySQL query statement below
+select product_id from Products
+where low_fats='Y' and recyclable='y';
