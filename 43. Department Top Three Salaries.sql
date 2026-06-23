90399b75dfb240969f033747803955e1976df3e7
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2026-06-23 10:04:29 +0530

    Update 43. Department Top Three Salaries.sql

--- 43. Department Top Three Salaries.sql
+++ 43. Department Top Three Salaries.sql
@@ -7,3 +7,11 @@ from employee e
 left join department d
 on e.departmentId=d.id) temp
 where ranks<=3;
+
+or 
+  Select d.name as department , e1.name as employee, e1.salary as Salary
+From Employee e1 join Department d on e1.DepartmentId = d.Id
+Where  3 > (select count(distinct (e2.Salary))
+            from  Employee e2
+            where e2.Salary > e1.Salary
+            and e1.DepartmentId = e2.DepartmentId)
